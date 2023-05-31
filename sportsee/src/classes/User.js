97b566a876e5f7f14ffed95c0a8c76628b5e4ee6
jbades <jbadespro@gmail.com>
2023-05-31 17:48:10 +0200

    mentor's refactoring; trying to display count block images

--- sportsee/src/classes/User.js
+++ sportsee/src/classes/User.js
@@ -1,45 +1,40 @@
+import extension from "../data/pathPatch.json"
+
+// const USE_MOCK = true; process.env.USE_MOCK
+const BASE_URL = "http://localhost:3001";
+
 export default class User {
     constructor (id) {
         this.id = id 
-        this.endpoint1 = {}
     }
-
-    
 
     fetchData(id) {
         return new Promise((resolve, reject) => {
-          const USE_MOCK = true;
-      
-          if (USE_MOCK) {
-            console.log("calling mock")  // to be deleted before release
-
-            const url = `/mock/user/${id}.json`
-            fetch(url)
-              .then(response => {
-                if (!response.ok) {
-                  throw new Error('Network response was not ok');
-                }
-                return response.json();
-              })
-              .then(data => {
-                this.endpoint1 = data
-                console.log(this.endpoint1)
-                resolve(data);
-              })
-              .catch(error => {
-                console.error('Error:', error);
-                reject(error);
-              });
-            } else {
-                console.log("calling API") // to be deleted before release
-
-                fetch(`http://localhost:3000/user/${id}`)
-                .then((response) => response.text())
-                .then((data) => {
-                    console.log(data)
-                    return data
+            const fetchPromises = Object.values(extension.patch).map((slug) => {
+                let url = `/mock/user/${id + slug}.json`;
+                if (!process.env.USE_MOCK) url = `${BASE_URL}/user/${id + slug}`;
+                
+                return fetch(url)
+                    .then((response) => {
+                        if (!response.ok) {
+                            throw new Error('Network response was not ok');
+                        }
+                        return response.json();
+                    });
+                });
+            
+            Promise.all(fetchPromises)
+                .then((results) => {
+                    const userData = {};
+                    results.forEach((data) => {
+                        Object.assign(userData, data.data);
+                    });
+                    resolve(userData);
                 })
-            }
-        });
-    }
+                .catch((error) => {
+                    console.error('Error:', error);
+                    reject(error);
+                });
+        })
+    }      
 }
--- sportsee/src/react/pages/Dashboard/Dashboard.jsx
+++ sportsee/src/react/pages/Dashboard/Dashboard.jsx
@@ -1,35 +1,58 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import User from "../../../classes/User";
+import CalorieCard from "../../components/CalorieCard";
+import caloriesData from "../../../data/caloriesData.json"
 
 function Dashboard() {
-  const { id } = useParams();
-  const [userData, setUserData] = useState(null);
+  const { id } = useParams()
+  const [userData, setUserData] = useState(null)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const user = new User(id);
-        const data = await user.fetchData(id);
-        setUserData(data);
+        const user = await new User(id)
+        const data = await user.fetchData(id)
+        setUserData(data)
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
-    };
+    }
 
-    fetchData();
-  }, [id]);
+    fetchData()
+  }, [id])
 
   if (!userData) {
-    return <div>Loading...</div>;
+    return <div className="loading-state">Loading...</div>
   }
 
-  const firstName = userData?.data?.userInfos?.firstName || "";
-
-  return <div>
-    <h1 className="h1--custom">Bonjour <span className="h1__firstname">{firstName}</span></h1>
-    <div>Félicitation ! Vous avez explosé vos objectifs hier</div>
-  </div>;
+  return <div className="dashboard__wrapper">
+    <div className="welcome__wrapper">
+      <h1 className="h1--custom">Bonjour <span className="h1__firstname">{userData.userInfos.firstName}</span></h1>
+      <div>Félicitation ! Vous avez explosé vos objectifs hier</div>
+    </div>
+    <div className="metrics__wrapper">
+      <div className="dynamic-metrics__wrapper">
+        Activité
+        <div className="">Durée - intentsité - score</div>
+      </div>
+      <div className="static-metrics__wrapper">
+        {Object.entries(userData.keyData).map(([key, value]) => {
+          if (Object.keys(caloriesData).includes(key)) {
+            return <CalorieCard 
+              key={key} 
+              dataKey={key} 
+              dataValue={value} 
+              img={caloriesData[key].img} 
+              unit={caloriesData[key].unit} name={caloriesData[key].name} 
+            />;
+          } else {
+            return null;
+          }
+        })}
+      </div>
+    </div>
+  </div>
 }
 
-export default Dashboard;
+export default Dashboard
